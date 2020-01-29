d847e5eaef09dc1d540666ea22b33696a7968d03
baseball19k <42750515+baseball19k@users.noreply.github.com>
2020-01-29 18:36:49 -0500

    updated

--- lab3/index.js
+++ lab3/index.js
@@ -2,11 +2,13 @@ const Express = require("express");
 const App = Express();
 const port = 8080;
 
-App.get('/', function(Request, Response) {
-        Response.send("Hello World!");
-});
 
-App.get('/public', function(Request, Response) {
+App.use(Express.static("public"));
+
+//App.get('/public', function(Request, Response) {
+  //      Response.send("Hello World!");
+//});
+App.get('/', function(Request, Response) {
         Response.send("Hello World!");
 });
 
