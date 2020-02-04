2cfabe32350fc1d003dc0747ebf5ed3e3d2fecd2
baseball19k <42750515+baseball19k@users.noreply.github.com>
2020-02-03 19:03:32 -0500

    we will see

---
+++ lab_4/index.js
@@ -0,0 +1,21 @@
+const Express  = require("express");
+const App = Express();
+const port = 80;
+
+const Pokemon = require
+
+App.get("/pokemon/id/:id", (req, res) => {
+    res.send(req.params.id);
+});
+
+App.get("/pokemon/name/:name", (req, res) => {
+    res.send(req.params.name);
+});
+
+App.get("/:id", (req, res) => {
+    res.send(req.params);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+});
