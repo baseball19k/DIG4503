9dfbf56b601bf3956c12cf6e7e6ced46a29b2f8f
baseball19k <42750515+baseball19k@users.noreply.github.com>
2020-02-05 18:36:42 -0500

    YAAAA

--- lab_4/index.js
+++ lab_4/index.js
@@ -2,7 +2,7 @@ const Express  = require("express");
 const App = Express();
 const port = 80;
 
-const Pokemon = require
+const Pokemon = require('json-pokemon')
 
 App.get("/pokemon/id/:id", (req, res) => {
     res.send(req.params.id);
