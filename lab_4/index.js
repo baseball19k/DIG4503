c19498b671e0875945bd6a9aa252e81b5e5b9650
baseball19k <jimmyclement10@gmail.com>
2020-02-08 18:25:21 -0500

    bet

--- lab_4/index.js
+++ lab_4/index.js
@@ -1,21 +1,44 @@
 const Express  = require("express");
 const App = Express();
 const port = 80;
+const chalk = require('chalk');
+const Pokemon = require('json-pokemon/getPokemon')
 
-const Pokemon = require('json-pokemon')
+App.get("/id/:id", (req, resp) => {
+    let reup = getPokemons.getPokemonById(parseInt(req.params.id));
 
-App.get("/pokemon/id/:id", (req, res) => {
-    res.send(req.params.id);
+    if (result === null) {
+        resp.send("<h2 style=\"color:red;\">Wrong</h2>");
+        console.log(chalk.red(getPath(req)));
+    } else {
+        console.log(chalk.green(getPath(req)));
+        resp.send(result);
+    }
 });
 
-App.get("/pokemon/name/:name", (req, res) => {
-    res.send(req.params.name);
+App.get("/name/:name", (req, resp) => {
+    let reup = getPokemons.getPokemonByName(req.params.name);
+
+    if (result === null) {
+        console.log(chalk.red(req.url));
+        resp.send(
+            '<h2>ERROR <span style="color:red;">' + req.params.name + "</span>" + " isn't a choice.</h2>"
+        );
+    } else {
+        resp.send(reup);
+        console.log(chalk.green(getPath(req)));
+    }
 });
 
-App.get("/:id", (req, res) => {
-    res.send(req.params);
+App.get("*", function (req, resp) {
+    resp.status(404).send("<h2 style=\"color:red\">404 - INVALID URL</h2>");
+    console.log(chalk.keyword(ERROR_COLOR)(getFullPath(req)));
 });
 
 App.listen(port, () => {
     console.log("Server running!");
 });
+
+function getPath(request) {
+    return request.protocol + '://' + request.get('host') + request.originalUrl;
+}
