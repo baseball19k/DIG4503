de8d3318730a23365968b4818901c16b9fc72fb7
baseball19k <jimmyclement10@gmail.com>
2020-02-15 20:22:18 -0500

    close

--- pracLab4/index.js
+++ pracLab4/index.js
@@ -4,29 +4,43 @@ const port = 80;
 const pokemons = require('json-pokemon');
 const pokemon = pokemons[0];
 
+App.use("/", Express.static("public"));
 
 App.get("/name/:name", (req, res) => {
     pokemons.forEach(value=> {
         if(value.name == req.params.name) {
-            res.send(value);
+            res.json(value);
             console.log(chalk.green(req.Path));
         }
     });
-    res.send("<h2 style=\"color:red;\">Invalid!</h2>");
+    res.send(result);
     console.log(chalk.red(req.Path));
 
 });
+
 App.get("/id/:id", (req, res) => {
     pokemons.forEach(value=> {
         if(value.id == req.params.id) {
-            res.send(value);
+            res.json(value);
             console.log(chalk.green(req.Path));
         }
     });
-    res.send("<h2 style=\"color:red;\">Invalid!</h2>");
+    res.send(result);
     console.log(chalk.red(req.Path));
 
 });
+
+App.get("/api/:value", (req, res) => {
+
+    pokemons.forEach(value=> {
+      if(value.value == req.params.value) {
+        res.json(result);
+        console.log(chalk.green(req.Path));
+      }
+    });
+    res.json(result);
+    console.log(chalk.red(req.Path));
+  });
 
 App.listen(port, () => {
     console.log("Server running!");
