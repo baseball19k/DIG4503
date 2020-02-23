7a9c4ef2b3fc92e0b8a82eaff7d85cd3bdd5ca28
baseball19k <jimmyclement10@gmail.com>
2020-02-22 21:51:27 -0500

    welp

---
+++ praclab6/index.js
@@ -0,0 +1,36 @@
+const Express = require("express");
+const App = Express();
+const chalk = require("chalk");
+const port = 80;
+const pokemons = require('json-pokemon');
+const pokemon = pokemons[0];
+const cors = require("cors");
+
+App.use(cors());
+App.use("/", Express.static("client/build"));
+
+App.get("/name/:name", (req, res) => {
+    let result = {"error": "That's not it chief."}
+
+    pokemons.forEach((value)=> {
+        if(value.name == req.params.name) {
+            result = value;
+        }
+    });
+    res.json(result);
+});
+
+App.get("/id/:id", (req, res) => {
+  let result = {"error": "That's not it chief."}
+
+    pokemons.forEach((value)=> {
+        if(value.id == req.params.id) {
+          result = value;
+        }
+    });
+    res.json(value);
+});
+
+App.listen(port, () => {
+    console.log("Server running!");
+});
